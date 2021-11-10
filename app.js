2fc714deaa515606cb674f7fb4b1b941bae76ec4
mgholden <mgholden27@gmail.com>
2021-11-09 23:34:33 -0500

    Started Cards, Working on CSS

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 
 //create 3 cards front and back
 //on-click to select cards
+const testCards = {
+    
+}
 //connect the left and right arrow keys for flipping through cards
 //create buttons to mark correct and incorrect
 //if card is on front left arrow key to move to back
