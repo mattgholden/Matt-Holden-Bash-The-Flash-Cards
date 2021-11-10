11a9fd7582c10620cfdd66d90bb59229fe024441
mgholden <mgholden27@gmail.com>
2021-11-09 20:04:08 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//console.log("Lets Play")
+
+
+//create 3 cards front and back
+//on-click to select cards
+//connect the left and right arrow keys for flipping through cards
+//create buttons to mark correct and incorrect
+//if card is on front left arrow key to move to back
+//if card is on back right arrow key to move to front
