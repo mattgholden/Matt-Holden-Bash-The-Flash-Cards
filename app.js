6589e28098b3d9f24b0ab077efad99eb578367f6
mgholden <mgholden27@gmail.com>
2021-11-16 22:24:09 -0500

    Buttons Functioning

--- app.js
+++ app.js
@@ -1,64 +1,84 @@
 //console.log("Lets Play")
-let correctCard = 0;
-let incorrectCard = 0;
+// const bashFlashCards = {
+    let correctCard = '.buttonCorrect' === 0
+    let incorrectCard = '.buttonIncorrect' == 9
 
 //Begin/opening for game
 
-// const bashFlashCards = {
-//     start: function(){
-//         alert(`Start Studying`)
-//         while(this.correctCard <= 9){
-//             this.studyBegin()
-//         }
-//     },
 
-//create 3 cards front and back for each category+
+    // start: function(){
+    //     alert(`Start Studying`)
+    //     while(this.correctCard < 9){
+    //     this.studyBegin()
+    //     }
+    // }
 
-//add on-click event listener to select cards
-    //     flashCard1.addEventListener('click',
     // studyBegin: function(){
-//flip card front and back function
+
+    //FLIP CARDs front and back function
     function flipCard(e){
     // console.log(e.target.children)
         e.currentTarget.children[0].classList.toggle('flipped')
         e.currentTarget.children[1].classList.toggle('flipped')
     }
+    // console.log(flipCard)
+        document.querySelectorAll('.card').forEach(card => card.addEventListener('click', flipCard))
 
-// console.log(flipCard)
-    document.querySelectorAll('.card').forEach(card => card.addEventListener('click', flipCard))
-
-
-//connect the left and right arrow keys for flipping through cards
-//create buttons to mark correct and incorrect
+ //BUTTONS to mark correct and incorrect
     // function clickButton(e){
-    //     e.currentTarget.children[0].classList.buttonClick.addEventListener('click,') 
-    //     e.target.children[1].classList.('click'
-    //     )
+    //     e.currentTarget.children[0].buttonClick.toggle('click') 
+    //     e.currentTarget.children[1].buttonClick.toggle('click')
     // }
-    // document.querySelectorAll('.button').forEach(button => button.addEventListener('click', clickButton))
+    // console.log(clickButton)
+        document.querySelectorAll('.buttonCorrect').forEach(button => button.addEventListener('click', function (){
+            alert(`Your answer is correct! Your score is ${correctCard += 1}.`)
+        }))
 
-//if card is on front left arrow key to move to back
-//if card is on back right arrow key to move to front
-//if 9 responses are correct you pass! Else try again.
+        document.querySelectorAll('.buttonIncorrect').forEach(button => button.addEventListener('click', function (){
+            alert(`Your answer is incorrect.  You have ${incorrectCard -= 1} more turns.`)
+        }))
 
-
-    // if (correctCard >= 9)
-    // else if (correctCard >= 9 && incorrectCard <= 8 
-        //anynumber )
-//array for cards being clicked
+        //  if (correctCard <= 9)
 
 //     }
 // }
+ // How to win.  If 9 attempts are correct you pass! Else try again.
 
-// bashFlashCards.start()
+    // correctCard () {
+    //     if (clickButton === ('.buttonCorrect') += 1)
+    //      alert(`Move to next card!`)
+    //     else if (clickButton === ('.buttonIncorrect') += 0)
+    // }
+     // correctCard >= 9)
+     //>= 9 && incorrectCard <= 8 
+        //anynumber )
 
-//define all variables
-// const flashCard1 = document.querySelector('.flash-card1')
-// const flashCard2 = document.querySelector('.flash-card2')
-// const flashCard3 = document.querySelector('.flash-card3')
-// const flashCard4 = document.querySelector('.flash-card4')
-// const flashCard5 = document.querySelector('.flash-card5')
-// const flashCard6 = document.querySelector('.flash-card6')
-// const flashCard7 = document.querySelector('.flash-card7')
-// const flashCard8 = document.querySelector('.flash-card8')
-// const flashCard9 = document.querySelector('.flash-card9')
+
+
+// bashFlashCards.start()        
+
+
+//Connect the left and right arrow keys for flipping through cards
+
+
+
+
+//if card is on front left arrow key to move to back
+//if card is on back right arrow key to move to front
+//array for cards being clicked
+
+
+
+// Define all variables
+        // const flashCard1 = document.querySelector('.flash-card1')
+        // const flashCard2 = document.querySelector('.flash-card2')
+        // const flashCard3 = document.querySelector('.flash-card3')
+        // const flashCard4 = document.querySelector('.flash-card4')
+        // const flashCard5 = document.querySelector('.flash-card5')
+        // const flashCard6 = document.querySelector('.flash-card6')
+        // const flashCard7 = document.querySelector('.flash-card7')
+        // const flashCard8 = document.querySelector('.flash-card8')
+        // const flashCard9 = document.querySelector('.flash-card9')
+
+//  STRETCH 
+// Users can create cards
